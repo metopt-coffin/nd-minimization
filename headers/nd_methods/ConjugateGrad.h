b0d1557733c1b6e4dd3974a14b8807a07549e5c8
Xagen37 <xagen.rus@gmail.com>
2021-04-13 20:51:55 +0300

    Add explaining comments

--- headers/nd_methods/ConjugateGrad.h
+++ headers/nd_methods/ConjugateGrad.h
@@ -11,11 +11,20 @@ struct ConjucateGrad : MinSearcher
     {}
 
 protected:
+    /*
+     * Find n-dimensional function's minimum
+     * using conjugate gradient method.
+     */
     SearchRes find_min_impl() override;
+    /*
+     * Find n-dimensional function's minimum
+     * using conjugate gradient method.
+     * Outputs tracing information.
+     */
     TracedSearchRes find_min_traced_impl() override;
 
 protected:
-    double m_eps;
+    double m_eps; // required precision
 };
 
 } // namespace min_nd
--- src/nd_methods/ConjugateGrad.cpp
+++ src/nd_methods/ConjugateGrad.cpp
@@ -8,12 +8,23 @@
 #include <iostream>
 
 namespace min_nd {
-
+/*
+ * Idea: calculate the steepest direction (antigradient); 
+ * compute coefficients alpha and beta, using give formulas;
+ * update the position;
+ * update the conjugate direction; 
+ * repeat until exit condition is satisfied.
+ *
+ * Note: with a pure quadratic function the minimum is reached within N iterations.
+ */
 SearchRes ConjucateGrad::find_min_impl()
 {
     const double eps_pow2 = m_eps * m_eps;
     auto & func = curr_func();
 
+    /*
+     * Initialize starting values.
+     */
     util::Vector curr(func.dims());
     auto grad = func.grad(curr);
     auto p = grad * -1.;
@@ -24,17 +35,21 @@ SearchRes ConjucateGrad::find_min_impl()
     uint iter_num = 0;
 
     while (grad_len_pow2 >= eps_pow2) {
+        /*
+         * "Restart" the method to avoid 
+         * accumulating computing error
+         */
         if (iter_num % func.dims() == 0) {
             beta = 0.;
         }
 
-        a_by_p = func.a() * p;
-        double alpha = grad_len_pow2 / (a_by_p * p);
+        a_by_p = func.a() * p; // compute A * p_k
+        double alpha = grad_len_pow2 / (a_by_p * p); // compute coefficient alpha
 
-        curr = curr + alpha * p;
-        grad = grad + alpha * a_by_p;
-        beta = grad.length_pow2() / grad_len_pow2;
-        p = beta * p - grad;
+        curr = curr + alpha * p; // update position
+        grad = grad + alpha * a_by_p; // update gradient
+        beta = grad.length_pow2() / grad_len_pow2; // compute coefficient beta
+        p = beta * p - grad; //update the conjugate vector
 
         grad_len_pow2 = grad.length_pow2();
         iter_num++;
@@ -43,6 +58,9 @@ SearchRes ConjucateGrad::find_min_impl()
     return {curr, func(curr)};
 }
 
+/*
+ * Version with tracing output.
+ */
 TracedSearchRes ConjucateGrad::find_min_traced_impl()
 {
     const double eps_pow2 = m_eps * m_eps;
