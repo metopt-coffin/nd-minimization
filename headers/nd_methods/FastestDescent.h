9603dee4da66641025a964b6faff4e994c57eb88
Xagen37 <xagen.rus@gmail.com>
2021-04-13 19:11:18 +0300

    Add explaining comments

--- headers/nd_methods/FastestDescent.h
+++ headers/nd_methods/FastestDescent.h
@@ -9,7 +9,7 @@
 
 namespace min_nd {
 
-struct FastestDescent : Gradient
+struct FastestDescent : Gradient    // derived from Gradient to reuse variables
 {
     FastestDescent(double eps, double max_step, min1d::MinSearcher & sd_searcher)
         : Gradient(eps, max_step)
@@ -17,18 +17,36 @@ struct FastestDescent : Gradient
     {}
 
 public:
+    /*
+     * Set current one dimensional minimization method.
+     */
     void set_sd_searcher(min1d::MinSearcher & sd_searcher) { m_sd_searcher = &sd_searcher; }
 
 protected:
+    /*
+     * Find n-dimensional function's minimum 
+     * using fastest descent method.
+     */
     SearchRes find_min_impl() override;
+    /*
+     * Find n-dimensional function's minimum
+     * using fastest descent method.
+     * Outputs tracing information.
+     */
     TracedSearchRes find_min_traced_impl() override;
 
 private:
+    /*
+     * Solve the one dimensional minimization problem.
+     */
     min1d::SearchRes find_sd_min(util::Function && func) { return m_sd_searcher->find_min(std::move(func)); }
+    /*
+     * Returns function for which one dimensional minimization problem was solved.
+     */
     const util::Function & last_sd_func() const noexcept { return m_sd_searcher->last_func(); }
 
 private:
-    min1d::MinSearcher * m_sd_searcher;
+    min1d::MinSearcher * m_sd_searcher;     // Current one dimensional minimization method
 };
 
 } // namespace min_nd
--- src/nd_methods/FastestDescent.cpp
+++ src/nd_methods/FastestDescent.cpp
@@ -8,17 +8,22 @@
 #include "util/VersionedData.h"
 
 namespace min_nd {
-
+/*
+ * Idea: after finding gradient of the function do not make a small step in the direction of the antigradient.
+ * Instead, move, until the function decreases. 
+ * After finding minimum on the chosen direction, find function's gradient again. 
+ * Repeat the algorithm.
+ */
 SearchRes FastestDescent::find_min_impl()
 {
     const double eps_pow2 = m_eps * m_eps;
     auto & func = curr_func();
 
-    util::Vector curr(func.dims());
+    util::Vector curr(func.dims()); // Vector of current coordinates
     double f_curr = func(curr);
 
     util::Vector grad = func.grad(curr);
-    min1d::SearchRes sd_min;
+    min1d::SearchRes sd_min;    // Minimum found on the chosen direction
     while (grad.length_pow2() >= eps_pow2) {
         sd_min = find_sd_min({[&](double x) { return func(curr - x * grad); }, {0., m_alpha}});
         curr = curr - sd_min.min_point * grad;
@@ -28,6 +33,9 @@ SearchRes FastestDescent::find_min_impl()
     return {curr, sd_min.min};
 }
 
+/*
+ * Version with tracing output.
+ */
 TracedSearchRes FastestDescent::find_min_traced_impl()
 {
     const double eps_pow2 = m_eps * m_eps;
